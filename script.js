275ce9096d9e667fe462ad23ee8294363bda0cd9
Zehra Çelik <zehra.celikk2002@gmail.com>
2023-08-18 23:09:01 +0300

    new pages were added.

--- script.js
+++ script.js
@@ -107,4 +107,20 @@ clearEl.addEventListener('click', e=>{
 
 dlEl.addEventListener('click', e=> {
     qrCode.download({name:'qr', extension:'png'});
-})
+});
+
+const sidebar = document.querySelector('.sidebar');
+const toggleButton = document.getElementById('sidebarToggle');
+
+toggleButton.addEventListener('click', () => {
+    sidebar.classList.toggle('open');
+});
+
+document.addEventListener('click', (event) => {
+    const isToggleOrSidebar = toggleButton.contains(event.target) || sidebar.contains(event.target);
+    const isCollapsed = sidebar.classList.contains('collapsed');
+
+    if (!isToggleOrSidebar && !isCollapsed) {
+        sidebar.classList.add('collapsed');
+    }
+});
