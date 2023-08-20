afca8f41265360e441f13f3b317a56cf3a7abef6
Zehra Çelik <zehra.celikk2002@gmail.com>
2023-08-21 00:04:08 +0300

    download button was changed and some bugs fixed

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const darkmodemoon = document.getElementById("darkmodemoon");
+    const body = document.body;
 
-var darkmodemoon = document.getElementById("darkmodemoon");
-darkmodemoon.onclick = function(){
-    document.body.classList.toggle("dark-theme");
-    if(document.body.classList.contains("dark-theme")){
-        darkmodemoon.src = "images/2995008_colors_colour_rainbow_colored_sun_icon.svg";
-    }else{
-        darkmodemoon.src = "images/9071075_dark_mode_icon.svg";
+
+    const darkModePreference = localStorage.getItem('darkModePreference');
+    if (darkModePreference === 'enabled') {
+    body.classList.add('dark-theme');
+    darkmodemoon.src = "images/2995008_colors_colour_rainbow_colored_sun_icon.svg";
     }
-}
+
+    darkmodemoon.onclick = function() {
+    body.classList.toggle("dark-theme");
+
+    if (body.classList.contains("dark-theme")) {
+        darkmodemoon.src = "images/2995008_colors_colour_rainbow_colored_sun_icon.svg";
+        localStorage.setItem('darkModePreference', 'enabled');
+    } else {
+        darkmodemoon.src = "images/9071075_dark_mode_icon.svg";
+        localStorage.setItem('darkModePreference', 'disabled');
+    }
+    };
+});
 
 const sidebar = document.querySelector('.sidebar');
 const toggleButton = document.getElementById('sidebarToggle');
